d62b4bddf03004d40d660219c821931dcbe05c15
Adam Lech <owenfan1646@gmail.com>
2012-09-18 00:41:19 -0400

    Commit

--- learningJava/testNode.java
+++
@@ -1,9 +0,0 @@
-public class testNode { 
-
-  public static void main(String[] args) { 
-     Node<String> me = new Node<String>("Adam"); 
-	 System.out.println(me.data()); 
-	 Node<String> meRef = new Node<String> ("John", me); 
-	 System.out.println(meRef.nextNode().data()); 
-  } 
-} 
